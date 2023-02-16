260129a4eadb0df3e30b488012a37bf493018757
Racheal <raychellewanjiru1021@gmail.com>
2023-02-16 09:08:27 -0800

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,9 @@
  * main - A program that prints a line using printf function
  * Retuern 0 (success)
  */
-{
+
 int main(void)
+{
 printf("with proper grammar, but the outcome is a piece of art, \n");
 return (0);
 }
