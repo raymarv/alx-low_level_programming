e8fc0238697008607515e1cad2d65a140e48572c
Racheal <raychellewanjiru1021@gmail.com>
2023-02-16 07:31:11 -0800

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A prodram that prints a line with put functions
+ *Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
