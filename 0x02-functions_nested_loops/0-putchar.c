289edbcf56979a60585c3a3aea5b8ceacc16339a
Racheal <raychellewanjiru1021@gmail.com>
2023-02-21 06:18:04 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,10 +11,10 @@ int main(void)
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
-       	_putchar('c');
+	_putchar('c');
 	_putchar('h');
 	_putchar('a');
-        _putchar('r');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
