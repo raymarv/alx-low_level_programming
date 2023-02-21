6a83ddb6361aa7fce45d984c9798c4a90177b1ba
Racheal <raychellewanjiru1021@gmail.com>
2023-02-21 07:10:15 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,14 @@
 
 void print_alphabet_x10(void);
 {
-        int j;
+	int j;
 	int x;
-	
+
 	for (x = 0; x < 10; x++)
-        {
-		for (j = 'a'; j <= 'z'; j++);
+	{
+		for (j = 'a'; j <= 'z'; j++)
 		{
-                _putchar(j);
+			_putchar(j);
 		}
 		_putchar('\n');
 	}
