23e7d47f0b6221f7d4adac7a3cd85310c142fa93
Racheal <raychellewanjiru1021@gmail.com>
2023-02-27 04:58:59 -0800

    hot fix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,15 @@
  */
 int main(void)
 {
-        char c;
+	char c;
 
-        c = 'a';
-        while
-                (c <='z') { putchar (c);
-                        c++;
-                }
-        putchar('\n');
-        return (0);
+	c = 'a';
+	while
+		(c <= 'z')
+		{
+			putchar (c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
 }
