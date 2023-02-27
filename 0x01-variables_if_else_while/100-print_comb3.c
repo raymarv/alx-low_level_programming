45dec27b5fb4306ed717a4c5e8b856a2ff3a0965
Racheal <raychellewanjiru1021@gmail.com>
2023-02-27 05:35:38 -0800

    hot fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
 	int d, p;
-	
+
 	for (d = '0'; d < '9'; d++)
 	{
 		for (p = d + 1; p <= '9'; p++)
