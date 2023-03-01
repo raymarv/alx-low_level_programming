030bd0e9501c6244481af8913164e7cb22c81723
Racheal <raychellewanjiru1021@gmail.com>
2023-03-01 12:06:12 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,11 +9,11 @@
  */
 char *_strcat(char *dest, char *src, int n)
 {
-	int index = x, dest_len = 0;
+	int index = n, dest_len = 0;
 
 	while (dest[index])
 		dest_len++;
-	for (index = 0, src[index] && index < n, index++)
+	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
 	return (dest);
 }
